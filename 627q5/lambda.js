ff1a4c06398dc4c96f525a703bd92d19b15f8093
HirudineeADRT <51395369+HirudineeADRT@users.noreply.github.com>
2019-08-15 18:10:43 +0530

    updated from git

--- 627q5/lambda.js
+++ 627q5/lambda.js
@@ -4,13 +4,15 @@ const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 exports.handler = function (event, context, callback) {
     cognito_idp.listUsers({
         UserPoolId: "us-east-1_uVXTQInep",
-        Limit: 10
+        Limit: 100
     }, function (error, data) {
         if (error) {
             // implement error handling logic here
+            //errot eeero
             throw error;
         }
-        // your logic goes within this block
+        // your logic goes within this block //change
+        //edit
     });
 
     callback(null, { "message": "Successfully executed" });
